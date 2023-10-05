a420c4c0584a9938ba704cb1d37f0bde8a1675f6
Jester Domingo <jrndomingo@gmail.com>
2023-10-05 13:26:31 -0400

    Switched to app.route | fixed styling

---
+++ src/routes/index.js
@@ -0,0 +1,32 @@
+// Importing express - creating Router
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/home', (req, res) => {
+    res.render('home', { title: 'Home' });
+  });
+  
+  router.get('/about', (req, res) => {
+    res.render('about', { title: 'About Me' });
+  });
+  
+  router.get('/contact', (req, res) => {
+    res.render('contact', { title: 'Contact' });
+  });
+  
+  router.get('/projects', (req, res) => {
+    res.render('projects', { title: 'Projects' });
+  });
+  
+  router.get('/services', (req, res) => {
+    res.render('services', { title: 'Services' });
+  });
+  
+
+  // POST route for handling the contact form submission
+router.post('/contact', (req, res) => {
+    res.send('Form submitted successfully');
+  });
+  
+  module.exports = router;
